5668269d44d1363279c393d82230788200b6fdc1
PMG2005 <149801162+PMG2005@users.noreply.github.com>
2023-11-10 17:10:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "particle.hpp"
 #include <ctime>
 
-int main() {
+void traccia2() {
   srand(time(NULL));
   gRandom->SetSeed();
   Particle::AddParticleType("pioni+", 0.13957, 1);
@@ -18,7 +18,8 @@ int main() {
 
   const int N = 120;
   Particle EventParticles[N];
-  int eventParticlesSize = 0;
+  int eventParticlesSize;
+  int decaycounter;
 
   // TCanvas *prova = new TCanvas("prova", "Istogrammi fino ad ora");
   // prova->Divide(3, 4);
@@ -28,36 +29,34 @@ int main() {
       new TH1F("h2", "Distribuzione angolo azimutale", 1000, 0, 2 * M_PI);
   TH1F *h3 = new TH1F("h3", "Distribuzione angolo polare", 1000, 0, M_PI);
   TH1F *h4 = new TH1F("h4", "Distribuzione impulso", 500, 0, 7);
-  TH1F *h5 = new TH1F("h5", "Distribuzione impulso trasverso", 500, 0, 5);
+  TH1F *h5 = new TH1F("h5", "Distribuzione impulso trasverso", 500, 0, 4.5);
   TH1F *h6 =
-      new TH1F("h6", "Distribuzione della energia delle particelle", 500, 0, 7);
+      new TH1F("h6", "Distribuzione della energia delle particelle", 500, 0, 6);
   TH1F *h7 = new TH1F("h7", "Massa invariante fra tutte le particelle generate",
-                      500, 0, 5);  // range e numero bin da decidere
-  h7->Sumw2();
+                      1000, 0, 6);  // range e numero bin da decidere
   TH1F *h8 = new TH1F("h8", "Massa invariante particelle segno discorde", 500,
-                      0, 5);  // range e numero bin da decidere
+                      0, 6);  // range e numero bin da decidere
   h8->Sumw2();
   TH1F *h9 = new TH1F("h9", "Massa invariante particelle segno concorde", 500,
-                      0, 5);  // range e numero bin da decidere
+                      0, 6);  // range e numero bin da decidere
   h9->Sumw2();
   TH1F *h10 = new TH1F("h10", "Massa invariante pioni kaoni segno discorde",
-                       500, 0, 5);  // range e numero bin da decidere
+                       500, 0, 6);  // range e numero bin da decidere
   h10->Sumw2();
   TH1F *h11 = new TH1F("h11", "Massa invariante pioni kaoni segno concorde",
-                       500, 0, 5);  // range e numero bin da decidere
+                       500, 0, 6);  // range e numero bin da decidere
   h11->Sumw2();
   TH1F *h12 = new TH1F("h12", "Massa invariante per k*", 500, 0.6, 1.2);
-  h12->Sumw2();
-  for (int j = 0; j < 1 ; ++j, eventParticlesSize = 0) {
-   
+  for (int j = 0; j < 1e5; ++j) {
+    eventParticlesSize = 0;
     int i = 0;
+    decaycounter = 0;
     for (i = 0; i < 100; ++i) {
       ++eventParticlesSize;
       Double_t type = gRandom->Rndm();
       Double_t phi = gRandom->Uniform(0, 2 * M_PI);
       Double_t theta = gRandom->Uniform(0, M_PI);
       Double_t p = gRandom->Exp(1);
-      int decaycounter = 0;
       if (type < 0.4)  // pioni+
       {
         EventParticles[i].SetIndex(0);
@@ -94,7 +93,9 @@ int main() {
                                      EventParticles[101 + decaycounter]);
         h12->Fill(EventParticles[100 + decaycounter].InvMass(
             EventParticles[101 + decaycounter]));
-        decaycounter++;
+        ++decaycounter;
+        ++decaycounter;
+
       }
       EventParticles[i].SetP(p * sin(theta) * cos(phi),
                              p * sin(theta) * sin(phi), p * cos(theta));
@@ -105,49 +106,43 @@ int main() {
       h5->Fill(p * sin(theta));
       h6->Fill(EventParticles[i].GetEnergy());
     }
-   
-    for (i = 0; i < eventParticlesSize && EventParticles[i].GetIndex() != 6;
-         ++i)  // diverso da 6 perchè vanno escluse le k*
-    {
-      for (int k = 0; k < i && EventParticles[k].GetIndex() != 6;
-           ++k)  // fuori dal ciclo di generazione per avere l'array già pieno,
-                 // forse si può implementare meglio
-      {  // per calcolare la massa inv. tra le part. i e k solo una volta
-        h7->Fill(EventParticles[i].InvMass(EventParticles[k]));
-        if ((EventParticles[i].GetIndex() % 2 == 1 &&
-             EventParticles[k].GetIndex() % 2 == 0) ||
-            (EventParticles[i].GetIndex() % 2 == 0 &&
-             EventParticles[k].GetIndex() % 2 ==
-                 1))  // particelle di segno discorde usando l'indice, si può
-                      // implementare meglio con GetCharge()
+    for (i = 0; i < eventParticlesSize; ++i) {
+      if (EventParticles[i].GetIndex() != 6) {
+        for (int k = 0; k < i; ++k)
+
         {
-          h8->Fill(EventParticles[i].InvMass(EventParticles[k]));
-          if (EventParticles[i].GetIndex() <= 3 &&
-              EventParticles[k].GetIndex() <= 3 &&
-              EventParticles[i].GetMass() !=
-                  EventParticles[k]
-                      .GetMass())  // verifico che si tratti di un pione e un
-                                   // kaone, so già che hanno segno discorde
-            h10->Fill(EventParticles[i].InvMass(EventParticles[k]));
-        }
-        if ((EventParticles[i].GetIndex() % 2 == 0 &&
-             EventParticles[k].GetIndex() % 2 == 0) ||
-            (EventParticles[i].GetIndex() % 2 == 1 &&
-             EventParticles[k].GetIndex() % 2 ==
-                 1))  // particelle di segno concorde usando l'indice, si può
-                      // implementare meglio con GetCharge()
-        {
-          h9->Fill(EventParticles[i].InvMass(EventParticles[k]));
-          if (EventParticles[i].GetIndex() <= 3 &&
-              EventParticles[k].GetIndex() <= 3 &&
-              EventParticles[i].GetMass() !=
-                  EventParticles[k]
-                      .GetMass())  // verifico che si tratti di un pione e un
-                                   // kaone, so già che hanno segno concorde
-            h11->Fill(EventParticles[i].InvMass(EventParticles[k]));
+          if (EventParticles[k].GetIndex() != 6) {
+            h7->Fill(EventParticles[i].InvMass(EventParticles[k]));
+            if ((EventParticles[i].GetIndex() % 2 == 1 &&
+                 EventParticles[k].GetIndex() % 2 == 0) ||
+                (EventParticles[i].GetIndex() % 2 == 0 &&
+                 EventParticles[k].GetIndex() % 2 == 1))
+
+            {
+              h8->Fill(EventParticles[i].InvMass(EventParticles[k]));
+              if (EventParticles[i].GetIndex() <= 3 &&
+                  EventParticles[k].GetIndex() <= 3 &&
+                  EventParticles[i].GetMass() != EventParticles[k].GetMass())
+
+                h10->Fill(EventParticles[i].InvMass(EventParticles[k]));
+            } else if ((EventParticles[i].GetIndex() % 2 == 0 &&
+                        EventParticles[k].GetIndex() % 2 == 0) ||
+                       (EventParticles[i].GetIndex() % 2 == 1 &&
+                        EventParticles[k].GetIndex() % 2 == 1))
+
+            {
+              h9->Fill(EventParticles[i].InvMass(EventParticles[k]));
+              if (EventParticles[i].GetIndex() <= 3 &&
+                  EventParticles[k].GetIndex() <= 3 &&
+                  EventParticles[i].GetMass() != EventParticles[k].GetMass())
+
+                h11->Fill(EventParticles[i].InvMass(EventParticles[k]));
+            }
+          }
         }
       }
     }
+    //std::cout << eventParticlesSize << std::endl;
   }
 
   TFile *Laboratorio2 = new TFile("Laboratorio2.root", "RECREATE");
