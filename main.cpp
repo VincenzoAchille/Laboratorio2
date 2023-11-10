9fe0e8dcb590f4141de5b3048f4db235ea29052c
camillofoni <149800489+camillofoni@users.noreply.github.com>
2023-11-10 17:11:48 +0100

    prononi to protoni

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ void traccia2() {
   Particle::AddParticleType("pioni-", 0.13957, -1);
   Particle::AddParticleType("kaoni+", 0.49367, 1);
   Particle::AddParticleType("kaoni-", 0.49367, -1);
-  Particle::AddParticleType("prononi+", 0.93827, 1);
-  Particle::AddParticleType("prononi-", 0.93827, -1);
+  Particle::AddParticleType("protoni+", 0.93827, 1);
+  Particle::AddParticleType("protoni-", 0.93827, -1);
   Particle::AddParticleType("k*", 0.89166, 0, 0.050);
   Particle::PrintArray();
 
